a23afbc31bb49017e2d72f0440035e1be02879ca
fufu717 <hiroyuki.kawai777@gmail.com>
2020-10-14 01:37:44 +0900

    新規ユーザー登録のバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   has_many :comments
   has_many :prototypes
+
+  validates :name, presence: true
+  validates :profile, presence: true
+  validates :occupation, presence: true
+  validates :position, presence: true
+
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
